d0759a199eb0897ae5a6394f42fe92d454059fad
zunjing.c <962724011@qq.com>
2021-10-24 21:04:53 +0800

    修改目录结构

---
+++ operators/table/src/main/java/com/bdilab/dataflow/mapper/DataSourceStatisticMapper.java
@@ -0,0 +1,17 @@
+package com.bdilab.dataflow.mapper;
+
+import com.baomidou.dynamic.datasource.annotation.DS;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.bdilab.dataflow.model.DataSourceStatistic;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author wh
+ * @version 1.0
+ * @date 2021/09/16
+ */
+@Repository
+@DS("mysql")
+public  interface DataSourceStatisticMapper extends BaseMapper<DataSourceStatistic> {
+
+}

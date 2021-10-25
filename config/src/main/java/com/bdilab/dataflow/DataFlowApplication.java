0febebd7efecd5ad4358150b08b7bbf0e2caa8ab
zunjing.c <962724011@qq.com>
2021-10-25 09:06:27 +0800

    类扫描配置

--- config/src/main/java/com/bdilab/dataflow/DataFlowApplication.java
+++ config/src/main/java/com/bdilab/dataflow/DataFlowApplication.java
@@ -1,8 +1,10 @@
 package com.bdilab.dataflow;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author zunjing chen
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @date 2021/10/24
  * Application Start Class
  */
+@ComponentScan("com.bdilab.dataflow")
+@MapperScan("com.bdilab.dataflow.mapper")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DataFlowApplication {
     public static void main(String[] args) {

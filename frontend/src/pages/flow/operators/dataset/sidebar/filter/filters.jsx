a156c9d4a0284552994d0b3fb8b3d95aac08644b
super刘 <454071433@qq.com>
2021-10-26 17:00:28 +0800

    feat: 代码重构初步完成

---
+++ frontend/src/pages/flow/operators/dataset/sidebar/filter/filters.jsx
@@ -0,0 +1,33 @@
+import {useState, useEffect} from 'react'
+import {} from 'antd'
+
+import IconFont from '../../../../../../font'
+import SidebarModal from './sidebarModal'
+
+function Filters(props){
+
+    const [visible, setVisible] = useState(false)
+
+    const expandSidebarItem = (e) => {
+        setVisible(true)
+        console.log(e)
+        // e.target
+    }
+
+    const closeSidebarModal = (e) => {
+        setVisible(false)
+    }
+
+    return (
+        <div className='hetu_sidebar_wrapper'>
+            <div  className='hetu_sidebar_item' onClick={expandSidebarItem} >
+                <IconFont type='hetu-shuxing' className='hetu_sidebar_item_icon' /> filter
+            </div>
+            {
+                visible && <SidebarModal data={props.data} setData={props.setData} filter={props.filter} setVisible={setVisible}/>
+            }
+        </div>
+    )
+}
+
+export default Filters

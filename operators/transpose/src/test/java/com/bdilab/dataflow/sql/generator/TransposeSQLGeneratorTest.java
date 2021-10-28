cc16f34330076246dda101c9a4b9d608a0b79adf
zunjing.c <962724011@qq.com>
2021-10-28 22:41:41 +0800

    add transpose test code

--- operators/transpose/src/test/java/com/bdilab/dataflow/sql/generator/TransposeSQLGeneratorTest.java
+++ operators/transpose/src/test/java/com/bdilab/dataflow/sql/generator/TransposeSQLGeneratorTest.java
@@ -47,6 +47,17 @@ public class TransposeSQLGeneratorTest {
         assertTrue(re.contains("avgIf"));
     }
     @Test
+    public void airTableTest(){
+        TransposeDescription transposeDescription = new TransposeDescription("transpose","airuuid",3000);
+        transposeDescription.setColumnIsNumeric(false);
+        transposeDescription.setColumn("city");
+        transposeDescription.setGroupBy(new String[]{"time"});
+        transposeDescription.setAttributeWithAggregationMap(ImmutableMap.<String,String>builder().put("PM2_5","sum").build());
+        List<String> columnValues = Lists.newArrayList("杭州","南京");
+        TransposeSQLGenerator transposeSQLGenerator = new TransposeSQLGenerator(transposeDescription,columnValues);
+        assertEquals(transposeSQLGenerator.generate(),"SELECT time,sumIf(PM2_5,city = '杭州' )  AS `杭州_sum_PM2_5` ,sumIf(PM2_5,city = '南京' )  AS `南京_sum_PM2_5`  FROM airuuid GROUP BY time LIMIT 3000");
+    }
+    @Test
     public void groupTest() {
        assertEquals(transposeSQLGenerator.group()," GROUP BY name");
     }

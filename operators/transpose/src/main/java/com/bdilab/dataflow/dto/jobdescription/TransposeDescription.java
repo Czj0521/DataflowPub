2aba510ed7a2c090b2c09c13f01b4e485b76efe4
zunjing.c <962724011@qq.com>
2021-10-31 22:56:38 +0800

    add transpose controller for test

--- operators/transpose/src/main/java/com/bdilab/dataflow/dto/jobdescription/TransposeDescription.java
+++ operators/transpose/src/main/java/com/bdilab/dataflow/dto/jobdescription/TransposeDescription.java
@@ -1,12 +1,15 @@
 package com.bdilab.dataflow.dto.jobdescription;
 
 import com.bdilab.dataflow.operator.dto.jobdescription.JobDescription;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.Map;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 /**
  * transpose's dto.
  *
@@ -14,20 +17,25 @@ import lombok.NoArgsConstructor;
  * @create: 2021-09-23
  * @description:
  **/
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(value = "TransposeDescription")
 public class TransposeDescription extends JobDescription {
   @NotEmpty
+  @ApiModelProperty(value = "横轴", required = true, example = "name")
   private String column;
   @NotNull
+  @ApiModelProperty(value = "横轴数据类型", required = true, example = "false/ture")
   private boolean columnIsNumeric;
   @NotNull
+  @ApiModelProperty(value = "group列名列表", required = true, example = "['name','age']")
   private String[] groupBy;
   @NotNull
+  @ApiModelProperty(value = "计算属性名称和对应聚合计算方式{'name':'sum',\n 'age':'average'}", required = true)
   private Map<String, String> attributeWithAggregationMap;
   @NotNull
+  @ApiModelProperty(value = "横轴值最大数量", required = true, example = "20（横轴最多20个值）")
   private int topTransposedValuesNum;
 
   public TransposeDescription(String jobType, String dataSource,

1d5cf53513819c9f45a4aa27cd0a1144fdd33c80
winmi <winmi_nong@163.com>
2021-11-01 09:50:45 +0800

    fix:层级遮挡

--- frontend/src/pages/flow/operators/Join/SideBar/index.tsx
+++ frontend/src/pages/flow/operators/Join/SideBar/index.tsx
@@ -2,11 +2,15 @@ import { FC } from 'react';
 import SideItem from '../../../components/SideItem';
 import JoinType from './JoinType';
 
+<<<<<<< 778f8c09b24f74f24980864340ace639fa65993d
 <<<<<<< 7ff9462e30d12722a64d89abe5f3aa45cc8441c2
 const SideBar: FC = () => {
 =======
 const SideBar: FC = (props) => {
 >>>>>>> refactor:SideItem
+=======
+const SideBar: FC = () => {
+>>>>>>> fix:层级遮挡
     return <div className="hetu_table_sidebar_wrapper">
         <SideItem name="join type" width={180} height={200}>
             <JoinType />

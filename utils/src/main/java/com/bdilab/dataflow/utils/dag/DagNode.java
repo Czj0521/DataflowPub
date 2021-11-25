7f81fd82bf0ef923bff373e8dd4d62df39b5a073
wh <1147528493@qq.com>
2021-11-25 17:21:03 +0800

    Fix dag.

--- utils/src/main/java/com/bdilab/dataflow/utils/dag/DagNode.java
+++ utils/src/main/java/com/bdilab/dataflow/utils/dag/DagNode.java
@@ -46,19 +46,6 @@ public class DagNode {
   private DagNode() {
   }
 
-//  /**
-//   * Constructor.
-//   *
-//   * @param nodeId node ID
-//   * @param nodeType node type
-//   */
-//  public DagNode(String nodeId, String nodeType, Object nodeDescription, Integer slotSize) {
-//    this.nodeId = nodeId;
-//    this.nodeDataSlots = new NodeDataSlot[slotSize];
-//    this.nodeType = nodeType;
-//    this.nodeDescription = nodeDescription;
-//  }
-
   public DagNode(DagNodeInputDto dagNodeInputDto){
     this.nodeId = dagNodeInputDto.getNodeId();
     JSONArray dataSources = ((JSONObject) dagNodeInputDto.getNodeDescription()).getJSONArray("dataSource");
@@ -69,5 +56,37 @@ public class DagNode {
     this.outputDataSlots = new ArrayList<>();
     this.nodeType = dagNodeInputDto.getNodeType();
     this.nodeDescription = dagNodeInputDto.getNodeDescription();
+  }
+
+  public Integer getInputSlotSize() {
+    return this.inputDataSlots.length;
+  }
+
+  public String getPreNodeId(int slotIndex) {
+    return this.inputDataSlots[slotIndex].getPreNodeId();
+  }
+
+  void setPreNodeId(int slotIndex, String preNodeId) {
+    this.inputDataSlots[slotIndex].setPreNodeId(preNodeId);
+  }
+
+  public List<String> getFilterId(int slotIndex) {
+    return this.inputDataSlots[slotIndex].getFilterId();
+  }
+
+  public String getDataSource(int slotIndex) {
+    return this.inputDataSlots[slotIndex].getDataSource();
+  }
+
+  void setDataSource(int slotIndex, String dataSource) {
+    this.inputDataSlots[slotIndex].setDataSource(dataSource);
+  }
+
+  public List<String> getAllDataSources() {
+    List<String> dataSources = new ArrayList<>();
+    for (InputDataSlot inputDataSlot : this.inputDataSlots) {
+      dataSources.add(inputDataSlot.getDataSource());
+    }
+    return dataSources;
   }
 }

0d6a995774cfeb5004e7f6ea846bad0a17745db2
mng <1379384689@qq.com>
2021-12-02 00:31:35 +0800

    联动接口

--- config/src/test/java/com/bdilab/dataflow/ProfileTest.java
+++ config/src/test/java/com/bdilab/dataflow/ProfileTest.java
@@ -1,38 +1,44 @@
 package com.bdilab.dataflow;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.bdilab.dataflow.dto.ProfilerDescription;
+import com.bdilab.dataflow.service.ProfilerService;
 import com.bdilab.dataflow.service.impl.ProfilerServiceImpl;
 import com.bdilab.dataflow.utils.PivotChartJobUtils;
 import com.bdilab.dataflow.utils.clickhouse.ClickHouseJdbcUtils;
+import com.bdilab.dataflow.utils.dag.DagNode;
+import com.bdilab.dataflow.utils.dag.dto.DagNodeInputDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProfileTest {
   @Autowired
   ClickHouseJdbcUtils clickHouseJdbcUtils;
   @Autowired
-  ProfilerServiceImpl profilerService;
+  ProfilerService profilerService;
   @Autowired
   PivotChartJobUtils pivotChartJobUtils;
   @Test
   public void test(){
     ProfilerDescription profilerDescription =new ProfilerDescription();
     profilerDescription.setJobType("profiler");
-    profilerDescription.setDataSource("dataflow.airuuid");
+    profilerDescription.setDataSource(new String[]{"dataflow.airuuid"});
     profilerDescription.setProfilerColumnList(new ArrayList<String>(Arrays.asList("time","city","AQI")));
-    String dataSource = profilerDescription.getDataSource();
-    List<String> columnNameList = profilerDescription.getProfilerColumnList();
-    List<Map<String, Object>> profiler = profilerService.getProfiler(profilerDescription);
+    System.out.println(profilerDescription);
+
+    DagNodeInputDto dagNodeInputDto = new DagNodeInputDto();
+    dagNodeInputDto.setNodeDescription(JSONObject.toJSON(profilerDescription));
+
+    DagNode dagNode = new DagNode(dagNodeInputDto);
+    System.out.println(dagNode);
+    List<Map<String, Object>> profiler = profilerService.getProfiler(dagNode,new HashMap<>());
     System.out.println(profiler);
 //    List<Map<String, Object>> columnType = profilerService.getColumnType(dataSource, columnNameList);
 //    List<Map<String, Object>> columnMaxMin = profilerService.getColumnInfo(dataSource, columnType);
--- operators/profiler/src/main/java/com/bdilab/dataflow/service/ProfilerService.java
+++ operators/profiler/src/main/java/com/bdilab/dataflow/service/ProfilerService.java
@@ -1,6 +1,15 @@
 package com.bdilab.dataflow.service;
 
 
+import com.bdilab.dataflow.utils.dag.DagNode;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Profiler operator service.
@@ -8,7 +17,12 @@ package com.bdilab.dataflow.service;
  * @author YuShaochao
  * @create 2021-11-11
  */
+@Api(tags = "ProfilerService")
 public interface ProfilerService {
+  @ApiOperation(value = "getProfiler")
+  public List<Map<String, Object>> getProfiler(
+      @ApiParam(name = "dagNode", value = "联动节点信息") DagNode dagNode,
+      @ApiParam(name = "preFilterMap", value = "filter字符串")  Map<Integer,StringBuffer> preFilterMap);
 
 
 

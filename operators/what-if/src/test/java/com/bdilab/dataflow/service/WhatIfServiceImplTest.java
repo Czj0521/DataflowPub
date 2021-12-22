3b3725afe3773a9791f0d359d8c6b59f5b790e28
wh <1147528493@qq.com>
2021-12-22 17:33:09 +0800

    Update what-if UT.

--- operators/what-if/src/test/java/com/bdilab/dataflow/service/WhatIfServiceImplTest.java
+++ operators/what-if/src/test/java/com/bdilab/dataflow/service/WhatIfServiceImplTest.java
@@ -1,12 +1,14 @@
 package com.bdilab.dataflow.service;
 
 import com.alibaba.fastjson.JSONObject;
+import com.bdilab.dataflow.common.consts.CommonConstants;
 import com.bdilab.dataflow.dto.jobdescription.WhatIfDescription;
 import com.bdilab.dataflow.dto.pojo.Expression;
 import com.bdilab.dataflow.dto.pojo.dependentvariable.DependentVariable;
 import com.bdilab.dataflow.dto.pojo.independentvariable.BaseIndependentVariable;
 import com.bdilab.dataflow.dto.pojo.independentvariable.EnumerationIndependentVariable;
 import com.bdilab.dataflow.dto.pojo.independentvariable.RangeIndependentVariable;
+import com.bdilab.dataflow.utils.clickhouse.ClickHouseManager;
 import com.bdilab.dataflow.utils.dag.DagNode;
 import com.bdilab.dataflow.utils.dag.dto.DagNodeInputDto;
 import java.util.ArrayList;
@@ -30,6 +32,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 class WhatIfServiceImplTest {
   @Resource
   WhatIfServiceImpl whatIfService;
+  @Resource
+  ClickHouseManager clickHouseManager;
 
   WhatIfDescription whatIfDescription = new WhatIfDescription();
   Expression expression = new Expression();
@@ -40,18 +44,21 @@ class WhatIfServiceImplTest {
       whatIfDescription.setCollectors(new String[]{"avg(trans1)", "avg(trans2)", "avg(AQI)", "count(trans2)"});
       whatIfDescription.setDataSource(new String[]{"dataflow.airuuid"});
       whatIfDescription.setJobType("base");
+
       DependentVariable dependentVariable1 = new DependentVariable("trans1", "($new1$ * 0.1) + AQI");
       DependentVariable dependentVariable2 = new DependentVariable("trans2", "$new2$ + AQI + trans1");
       expression.getDependentVariables().add(dependentVariable1);
       expression.getDependentVariables().add(dependentVariable2);
-      BaseIndependentVariable integerEnumeration1 = new EnumerationIndependentVariable("$new1$", "0",new ArrayList<String>(){{
+
+      BaseIndependentVariable independentVariable1 = new EnumerationIndependentVariable("$new1$", "0",new ArrayList<String>(){{
         this.add("2");
         this.add("4");
         this.add("6");
       }});
-      BaseIndependentVariable integerEnumeration2 = new RangeIndependentVariable("$new2$", "0", "0", "100", "10");
-      expression.getIndependentVariables().add(integerEnumeration1);
-      expression.getIndependentVariables().add(integerEnumeration2);
+      BaseIndependentVariable independentVariable2 = new RangeIndependentVariable("$new2$", "0", "0", "100", "10");
+      expression.getIndependentVariables().add(independentVariable1);
+      expression.getIndependentVariables().add(independentVariable2);
+
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -90,6 +97,7 @@ class WhatIfServiceImplTest {
     DagNode dagNode = new DagNode(new DagNodeInputDto("whatIf_Test", "what_if", JSONObject.toJSON(whatIfDescription)));
     try{
       whatIfService.saveToClickHouse(dagNode, expression);
+      clickHouseManager.deleteTable(CommonConstants.CPL_TEMP_TABLE_PREFIX + "whatIf_Test");
     } catch (Exception e) {
       throw new RuntimeException("Test - Error： testSaveToClickHouse !\n" + e.getMessage());
     }
